7476f177553da099ca8840e86baabfc757fc33a9
Tom Johnson <tomjoht@gmail.com>
2021-09-13 09:18:26 -0700

    slides for presentation tekom china

--- upload.sh
+++ upload.sh
@@ -1,4 +1,4 @@
-aws s3 cp ~/projects/learnapidoc/slides/images/$1 s3://idbwmedia.com/images/api/ --profile wasabi
+aws s3 cp ~/projects/learnapidoc/images/$1 s3://idbwmedia.com/images/api/ --profile wasabi
 # to run, store image in project's image folder
 # then type this: . upload.sh image.png
 echo '<a href=""><img src="https://s3.us-west-1.wasabisys.com/idbwmedia.com/images/api/'$1
